306ae048949c5ac6aab3ec9466ca52ada753881a
Aayush3cluz <37142737+Aayush3cluz@users.noreply.github.com>
2022-06-21 13:30:00 +1000

    push

---
+++ src/app/error-alert/error-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-alert',
+  templateUrl: './error-alert.component.html',
+  styleUrls: ['./error-alert.component.css'],
+})
+export class ErrorAlertComponent implements OnInit {
+  errorMessage = 'Something went wrong';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
